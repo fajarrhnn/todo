acb80030f0d15a7dc7409b574484b225a7383b96
fajarrhnn <fajarrehan023@gmail.com>
2024-07-08 12:40:13 +0700

    Redesign page of null todo

--- src/content/null.tsx
+++ src/content/null.tsx
@@ -3,9 +3,11 @@ import Title from "../components/title";
 
 export default function NullTodo() {
   return (
-    <main className="max-w-md h-max text-center w-auto space-y-4">
-      <Title>You don't have any tasks.</Title>
-      <Description>Please add a new task</Description>
+    <main className="flex justify-center items-center w-full h-screen">
+      <div className="max-w-md h-max text-center w-auto space-y-4">
+        <Title>You don't have any tasks.</Title>
+        <Description>Please add a new task</Description>
+      </div>
     </main>
   );
 }
